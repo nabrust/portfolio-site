348f01d28d0f6b1a46e4f41ef96c7674625a2f48
Rustam Nabiev <nab.rf@ya.ru>
2015-07-21 12:40:07 +0500

    ДЗ “Сайт портфолио” #1

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+
+$(".link-by-main-navigation").mouseover(function(){
+	$(this).addClass("hover");
+});
+
+$(".link-by-main-navigation").mouseout(function(){
+	$(this).removeClass("hover");
+});
+
+$(".link-by-main-navigation").mousedown(function() {
+	$(".link-by-main-navigation").removeClass("active");
+	$(this).addClass("active");
+});
