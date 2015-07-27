247e49fe67c163566e44b995a84a6750c9684bed
Rustam Nabiev <nab.rf@ya.ru>
2015-07-27 17:10:29 +0500

    Сделал поправки, и добавил разметку для popup

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,10 @@ gulp.task("watch", function() {
 		"app/my_work/css/**/*.css",
 		"app/contact_me/*.html",
 		"app/contact_me/js/**/*.js",
-		"app/contact_me/css/**/*.css"
+		"app/contact_me/css/**/*.css",
+		"app/login/*.html",
+		"app/login/js/**/*.js",
+		"app/login/css/**/*.css"
 	]) .on("change", browserSync.reload);
 });
 
