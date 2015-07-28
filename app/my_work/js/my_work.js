6c730e365ef4899cbc1b6775b9c730d97831c571
Rustam Nabiev <nab.rf@ya.ru>
2015-07-29 04:24:53 +0500

    Доработал страницы

---
+++ app/my_work/js/my_work.js
@@ -0,0 +1,7 @@
+$(".box_by_img_site").mouseover(function(){
+	$(this).children(".background_hover").css("display","block");
+});
+$(".box_by_img_site").mouseout(function(){
+	$(this).children(".background_hover").css("display","none");
+});
+
