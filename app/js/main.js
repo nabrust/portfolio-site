0d5d047fea261756540d65e09013ab1697f2fc7c
Rustam Nabiev <nab.rf@ya.ru>
2015-08-08 07:16:56 +0500

    сделал поддержку в IE8

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,19 @@
+$(function() {
+	$(".link-by-main-navigation").mouseover(function(){
+		$(this).addClass("hover");
+	});
 
-$(".link-by-main-navigation").mouseover(function(){
-	$(this).addClass("hover");
-});
+	$(".link-by-main-navigation").mouseout(function(){
+		$(this).removeClass("hover");
+	});
 
-$(".link-by-main-navigation").mouseout(function(){
-	$(this).removeClass("hover");
-});
+	$(".link-by-main-navigation").mousedown(function() {
+		$(".link-by-main-navigation").removeClass("active");
+		$(this).addClass("active");
+	});
 
-$(".link-by-main-navigation").mousedown(function() {
-	$(".link-by-main-navigation").removeClass("active");
-	$(this).addClass("active");
+	if (!Modernizr.input.placeholder){
+		$('input, textarea').placeholder({ customClass: 'my-placeholder' });
+	}
+
 });
